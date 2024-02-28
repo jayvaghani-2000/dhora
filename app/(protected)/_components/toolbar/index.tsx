69061fbd710d1feb03da66c0ed65e988845765bc
Jay vaghani <jayvaghani2000@gmail.com>
2024-02-28 14:14:50 -0500

    toolbar + mobile navbar fix

--- app/(protected)/_components/toolbar/index.tsx
+++ app/(protected)/_components/toolbar/index.tsx
@@ -1,7 +1,5 @@
-import clsx from "clsx";
 import { GoBell } from "react-icons/go";
-import { RiMenu5Fill } from "react-icons/ri";
-import { MdOutlineSearch } from "react-icons/md";
+import { MobileToggle } from "../navbar/mobileToggle";
 
 export interface Props {
   open: boolean;
@@ -11,45 +9,17 @@ export interface Props {
 export default function Toolbar({ open, handleToggleNav }: Props) {
   return (
     <>
-      <button
-        className={clsx({
-          "block absolute inset-0 top-[48px] backdrop-blur-sm	z-[75]": open,
-          hidden: !open,
-          "md:hidden": true,
-        })}
-        onClick={handleToggleNav}
-      />
-      <div className="hidden md:flex h-12 bg-primary-light-gray sticky top-0 z-20 px-2 items-center border-b-2 border-secondary-black gap-2">
-        <button className="relative after:content-[''] after:absolute after:h-2 after:w-2 after:rounded-full after:bg-[#FF0000] after:top-0">
+      <div className="text-md bg-primary-light-gray font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+        <MobileToggle />
+        <button className="relative after:content-[''] after:absolute after:h-2 after:w-2 after:rounded-full after:bg-[#FF0000] after:top-0 mr-1">
           <GoBell color="#b8b8b8" size={24} />
         </button>
-        <div className="w-0.5 bg-divider h-8" />
+        <div className="w-0.5 bg-divider h-8 ml-2" />
         {/* {activePage ? (
           <div className="flex items-center gap-1 font-semibold">
             {activePage.icon} {activePage.title}
           </div>
         ) : null} */}
-      </div>
-
-      <div className="md:hidden bg-primary-light-gray p-2.5 flex sticky top-0 justify-between items-center z-[100] h-12">
-        <button onClick={handleToggleNav}>
-          <RiMenu5Fill size={24} color="#b8b8b8" />
-        </button>
-        <button className="absolute after:content-[''] after:absolute after:h-2 after:w-2 after:rounded-full after:bg-[#FF0000] after:top-0">
-          <GoBell color="#b8b8b8" size={24} />
-        </button>
-
-        <div className="flex items-center gap-1 font-semibold">
-          {/* {activePage ? (
-            <>
-              <span>{activePage.icon}</span> <span>{activePage.title}</span>
-            </>
-          ) : null} */}
-        </div>
-
-        <div className="w-9">
-          <MdOutlineSearch size={24} color="#b8b8b8" />
-        </div>
       </div>
     </>
   );
