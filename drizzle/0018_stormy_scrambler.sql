7b6b716dfe24ac8cf04dc8f07340be7eba6a68cd
Jay vaghani <jayvaghani2000@gmail.com>
2024-04-12 17:05:35 +0530

    delete migration file

--- drizzle/0018_stormy_scrambler.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "business" ALTER COLUMN "user_id" SET DATA TYPE text;
--- drizzle/0019_married_vector.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "users" DROP CONSTRAINT "users_email_unique";
-
-CREATE UNIQUE INDEX “users_email_unique”
-ON users(email, deleted)
-WHERE deleted = false;
